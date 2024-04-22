de24b8ab2c77048c514a71ff0a7a200d79e69a65
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-04-22 16:20:33 +0200

    make Header and Hero

---
+++ src/components/Header.svelte
@@ -0,0 +1,10 @@
+<header class="flex justify-between items-center p-4 bg-gray-200">
+  <h1 class="text-xl font-bold">Bucket Dashboards</h1>
+  <a
+    href="https://github.com/CooperBigFoot/dashboard"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <i class="fab fa-github text-xl"></i>
+  </a>
+</header>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+</script>
+
+<div class="pt-10 pt-hero min-h-screen bg-base-200">
+  <h1
+    class="flex flex-col text-center text-5xl font-semibold sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl"
+  >
+    Visualize your <span class="text-blue-500">bucket model</span> run with a simple
+    click
+  </h1>
+  <div class="pt-10 flex justify-center items-center">
+    <form class="flex w-full max-w-sm items-center space-x-2">
+      <Input type="email" placeholder="upload .csv" />
+      <Button type="submit">Visualise</Button>
+    </form>
+  </div>
+</div>
